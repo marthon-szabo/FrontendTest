6d4d4815d3669980cc682c0e832926fcf986f7f8
Márton Szabó <makotoimx@gmail.com>
2020-10-20 11:00:24 +0200

    Add NavBar

---
+++ test_app/src/layouts/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavBar } from "react-bootstrap";
+
+export default function NavBar() {
+	return (
+		<div>
+			<Navbar bg="primary" variant="dark">
+				<Navbar.Brand href="#home">FrintednTest</Navbar.Brand>
+			</Navbar>
+		</div>
+	);
+}
