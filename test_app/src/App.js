f1584a0ea91d4d1b4761bf6b2d9499bf40b6e332
Márton Szabó <makotoimx@gmail.com>
2020-10-20 11:53:25 +0200

    Install react-router

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import logo from "./logo.svg";
 import NavBarLayout from "./layouts/NavBar";
 import Button from "react-bootstrap/Button";
+import styled from "styled-components";
+import Home from "./components/Home";
+import { Route } from "react-router";
 
 function App() {
 	return (
 		<div>
-			<header>
-				<NavBarLayout />
-			</header>
+			<NavBarLayout />
+			<Route exact path="/">
+				<Home />
+			</Route>
 		</div>
 	);
 }
